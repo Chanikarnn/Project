78f9c2ba6454ae829ab4ce40a6a2077e146fe8ea
Chanikarnn <95094960+Chanikarnn@users.noreply.github.com>
2022-03-20 22:40:45 +0700

    74

---
+++ src/views/About.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="about">
+    <v-container>
+      <p class="display-3 font-weight-light	text-center pa-4">แฟชั่นมาแรง !!!</p>
+        <div class="row">
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <v-card
+              class="mx-auto"
+              max-width="400"
+              outlined
+              tile
+              href="/post"
+            >
+              <v-img
+                class="white--text align-end"
+                height="400px"
+                :src="require('../assets/img/home/slider2.jpg')"
+              >
+                <v-card-title>แซ่บไฟลุก💥รวม 50ไอเดียแฟชั่นมาแรง </v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pb-0">ไอเดียแฟชั่น Bra T</v-card-subtitle>
+
+              <v-card-text class="text--primary">
+
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh venenatis cras sed felis eget velit aliquet sagittis id. Enim praesent elementum facilisis leo vel fringilla est ullamcorper.
+
+                </div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn
+                  color="orange"
+                  text
+                >
+                  Read More
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </div>
+          <div class="col-md-4 col-sm-3 col-xs-12">
+            <v-card
+              class="mx-auto"
+              max-width="400"
+              outlined
+              tile
+              href="/post"
+            >
+              <v-img
+                class="white--text align-end"
+                height="400px"
+                :src="require('../assets/img/home/slider3.jpg')"
+              >
+                <v-card-title>แซ่บไฟลุก💥รวม 50ไอเดียแฟชั่นมาแรง </v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pb-0">ไอเดียแฟชั่น Bra Top</v-card-subtitle>
+
+              <v-card-text class="text--primary">
+
+                <div> Bra Top ให้ลุคสาวเซ็กซี่ มั่นใจ ในสไตล์สายสตรีทสุดเท่ สายแซ่บเชิญทางนี้ มาส่องแฟชั่นสุดฮอตกับ 50 ไอเดียแฟชั่น "Bro Tpp" ให้ลุคสาวเซ็กซี่มั่นใจ ในสไตล์สายสตรีทสุดในบทความนี้กัน
+
+                </div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn
+                  color="orange"
+                  text
+                >
+                  Read More
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </div>
+          <div class="col-md-4 col-sm-3 col-xs-12">
+            <v-card
+              class="mx-auto"
+              max-width="400"
+              outlined
+              tile
+              href="/post"
+            >
+              <v-img
+                class="white--text align-end"
+                height="400px"
+                :src="require('../assets/img/home/slider4.jpg')"
+              >
+                <v-card-title>Fashion Tips & Tricks</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pb-0">Lorem ipsum</v-card-subtitle>
+
+              <v-card-text class="text--primary">
+
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh venenatis cras sed felis eget velit aliquet sagittis id. Enim praesent elementum facilisis leo vel fringilla est ullamcorper.
+
+                </div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn
+                  color="orange"
+                  text
+                >
+                  Read More
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </div>
+        </div>
+    </v-container>
+  </div>
+</template>
